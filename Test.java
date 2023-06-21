583725b0cc47be3efb81d0a21bb93c1727fb779a
Mahesh-Reddy69 <137245697+Mahesh-Reddy69@users.noreply.github.com>
2023-06-21 11:08:26 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,18 @@
+class Test {
+
+}
+
+class Main {
+  public static void main(String[] args) {
+
+    System.out.println("execution starting");
+
+    // create an object of the Test class
+    Test obj = new Test();
+
+    // print the object
+    System.out.println(obj);
+
+    System.out.println("execution ends");
+  }
+}
